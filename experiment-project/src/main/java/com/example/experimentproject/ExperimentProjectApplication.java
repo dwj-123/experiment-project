e7b864cdb3e9b0c7bca2b42d6bb7d83e7d029c0b
dingwenjie <16638139549@163.com>
2023-03-21 17:19:16 +0800

    Initial commit

---
+++ experiment-project/src/main/java/com/example/experimentproject/ExperimentProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.experimentproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExperimentProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExperimentProjectApplication.class, args);
+    }
+
+}
---
+++ experiment-project/src/test/java/com/example/experimentproject/ExperimentProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.experimentproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExperimentProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
